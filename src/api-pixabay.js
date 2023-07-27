f74c777f1dbb224dad3e016c01c0d9680f4b11f0
Ivanna86Koval <ms.ivanna86@gmail.com>
2023-07-27 14:03:58 +0300

    update

--- src/api-pixabay.js
+++ src/api-pixabay.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/api/';
 
 const API_KEY = '38494803-b3b4c85f97fe971422a222a26';
+
 const QUERY_OPT = 'image_photo&orientation=horizontal&safesearch=true';
 
 async function getQuery(value, page, per_page) {
@@ -13,3 +14,11 @@ async function getQuery(value, page, per_page) {
 }
 
 export { getQuery };
+
+/*axios.defaults.baseURL = `https://pixabay.com/api/`;
+
+export async function searchByQuery(query, page) {
+  return axios.get(
+    `?key=${BASE_URL}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
+  );
+}*/
